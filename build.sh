afdea4d73dab6e70edbe0d635d27d13544ff451e
Nikita Tarasov <nikita@mygento.ru>
2016-04-18 18:01:33 +0300

    fix

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ sed -i -e s/PHP_VERSION/${PHP_VERSION}/g module.yaml
 sed -i -e s/PHP_VERSION/${PHP_VERSION}/g fpm/module.yaml
 sed -i -e s/PHP_VERSION/${PHP_VERSION}/g wheezy/module.yaml
 sed -i -e s/PHP_VERSION/${PHP_VERSION}/g wheezy/fpm/module.yaml
-DR = "--rm=true --pull=true --force-rm=true --no-cache=true"
-if[ "$PHP_FPM" = true];
+DR="--rm=true --pull=true --force-rm=true --no-cache=true"
+if[ "$PHP_FPM" = true]
 then
     PHP_COMB = "$PHP_VERSION-fpm"
 else
@@ -21,7 +21,7 @@ then
 fi
 if [ $PHP_VERSION != '5.5' ]
 then
-    if[ "$PHP_FPM" = true];
+    if[ "$PHP_FPM" = true]
     then
         docker build $DR --tag=mygento/php:$PHP_VERSION-fpm fpm;
     else
