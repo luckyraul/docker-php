eb422be5df5c0299449ebf1c57ca3d9fea8eaadc
Nikita Tarasov <nikita@mygento.ru>
2016-04-19 22:48:31 +0300

    downgrade

--- build.sh
+++ build.sh
@@ -5,6 +5,15 @@ if [[ "$PHP_FPM" = "true" ]]; then
 else
     echo 'CLI version'
 fi;
+
+if [[ $PHP_VERSION = '5.5' ]]; then
+    if [[ "$PHP_FPM" = "true" ]]; then
+        echo "php::manage_repos: true" >> fpm/module.yaml
+    else
+        echo "php::manage_repos: true" >> module.yaml
+    fi;
+fi;
+
 if [[ "$PHP_FPM" = "true" ]]; then
     sed -i -e s/PHP_VERSION/${PHP_VERSION}/g fpm/module.yaml
     docker build $DR --tag=mygento/php:$PHP_TAG fpm
