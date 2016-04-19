2eb59ce63b6ea959d884f35aa111141cf2d683f0
Nikita Tarasov <nikita@mygento.ru>
2016-04-19 22:43:01 +0300

    downgrade

--- build.sh
+++ build.sh
@@ -5,21 +5,10 @@ if [[ "$PHP_FPM" = "true" ]]; then
 else
     echo 'CLI version'
 fi;
-if [[ $PHP_VERSION = '5.5' ]]; then
-    if [[ "$PHP_FPM" = "true" ]]; then
-        sed -i -e s/PHP_VERSION/${PHP_VERSION}/g wheezy/fpm/module.yaml
-        docker build $DR --tag=mygento/php:$PHP_TAG wheezy/fpm
-    else
-        sed -i -e s/PHP_VERSION/${PHP_VERSION}/g wheezy/module.yaml
-        docker build $DR --tag=mygento/php:$PHP_TAG wheezy
-    fi;
-fi;
-if [[ $PHP_VERSION != '5.5' ]]; then
-    if [[ "$PHP_FPM" = "true" ]]; then
-        sed -i -e s/PHP_VERSION/${PHP_VERSION}/g fpm/module.yaml
-        docker build $DR --tag=mygento/php:$PHP_TAG fpm
-    else
-        sed -i -e s/PHP_VERSION/${PHP_VERSION}/g module.yaml
-        docker build $DR --tag=mygento/php:$PHP_TAG .
-    fi
+if [[ "$PHP_FPM" = "true" ]]; then
+    sed -i -e s/PHP_VERSION/${PHP_VERSION}/g fpm/module.yaml
+    docker build $DR --tag=mygento/php:$PHP_TAG fpm
+else
+    sed -i -e s/PHP_VERSION/${PHP_VERSION}/g module.yaml
+    docker build $DR --tag=mygento/php:$PHP_TAG .
 fi
