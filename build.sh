f979d9fa9595d3a23aa9c239449e5dbcf2854e37
Nikita Tarasov <nikita@mygento.ru>
2016-12-21 18:25:19 +0300

    full

--- build.sh
+++ build.sh
@@ -1,14 +1,23 @@
 #/bin/bash
 DR="--rm=true --pull=true --force-rm=true --no-cache=true"
 if [[ "$PHP_FPM" = "true" ]]; then
-    echo 'FPM version'
+    if [[ "$PHP_FPM" = "true" ]]; then
+      echo 'FPM version'
+    else
+      echo 'FPM FULL version'
+    fi;
 else
     echo 'CLI version'
 fi;
 
 if [[ "$PHP_FPM" = "true" ]]; then
     sed -i -e s/PHP_VERSION/${PHP_VERSION}/g fpm/module.yaml
-    docker build $DR --tag=mygento/php:$PHP_TAG fpm
+    if [[ "$PHP_FULL" = "true" ]]; then
+      sed -i -e s/PHP_VERSION/${PHP_VERSION}/g full/module.yaml
+      docker build $DR --tag=mygento/php:$PHP_TAG full
+    else
+      docker build $DR --tag=mygento/php:$PHP_TAG fpm
+    fi;
 else
     sed -i -e s/PHP_VERSION/${PHP_VERSION}/g module.yaml
     docker build $DR --tag=mygento/php:$PHP_TAG .
